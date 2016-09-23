a41d6381556327ed711ebe5ec6033def0128ad9d
Dalton Claybrook <daltonclaybrook@gmail.com>
2016-09-23 00:58:50 -0500

    Added documentation to KVC.swift

--- TweenController/KVC.swift
+++ TweenController/KVC.swift
@@ -56,6 +56,17 @@ extension CGFloat: ObjectConvertible {
 }
 
 extension TweenPromise where T : ObjectConvertible {
+    
+    /// Instead of using an action block with a tween operation, you can
+    /// call this method to apply the value directly to an object using
+    /// Key-Value-Coding. For example:
+    ///
+    ///     controller.tween(from: 0.0, at: 0.0)
+    ///         .to(M_PI * 8.0, at: 1.0)
+    ///         .with(object: tweenView.layer, keyPath: "transform.rotation.z")
+    ///
+    /// - parameter object:  The object to apply tweened values to.
+    /// - parameter keyPath: The key-path on `object` used when applying tweened values.
     public func with(object: NSObject, keyPath: String) {
         with { [weak object] tweenable in
             object?.setValue(tweenable.toObject(), forKeyPath: keyPath)
